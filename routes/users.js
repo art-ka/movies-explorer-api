0346e9a2e9919de5f58bd514dfd945f515eb5b99
Katya Novikova <katy.novikova@gmail.com>
2021-03-18 15:29:10 +0200

    style: delete check authorization

--- routes/users.js
+++ routes/users.js
@@ -11,9 +11,6 @@ routerUser.get('/me', getCurrentUser);
 
 // обновляет информацию о пользователе (email и имя)
 routerUser.patch('/me', celebrate({
-  headers: Joi.object().keys({
-    authorization: Joi.string().required().min(100),
-  }).unknown(true),
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
     email: Joi.string().required().min(3).email(),
